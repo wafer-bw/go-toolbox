8663c246c9a870259a1bb7e40bda761203efc867
Ben Wakeford <ben.jz.wakeford@gmail.com>
2024-06-29 19:15:25 -0400

    Touch up comment

--- graceful/graceful.go
+++ graceful/graceful.go
@@ -32,8 +32,8 @@ type Group []Runner
 // [signal.NotifyContext], or the context provided to it is canceled, then
 // returns the first non-nil error (if any) or nil if a signal was received.
 //
-// An error returned from Start does not mean that all runners have stopped,
-// you must still call [Stop] for that.
+// An error returned from Start does not indicate that all runners have stopped,
+// you must call [Group.Stop] to stop all runners.
 func (g Group) Start(ctx context.Context, signals ...os.Signal) error {
 	eg, errCtx := errgroup.WithContext(ctx)
 	signalCtx, stop := signal.NotifyContext(ctx, signals...)
