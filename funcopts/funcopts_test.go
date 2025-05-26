fdf9a5acda7e8de1e9bb0f48e5e9655b1b3082fb
Ben Wakeford <ben.jz.wakeford@gmail.com>
2025-05-26 19:19:45 -0400

    Add funcopts module (#26)
    Resolves: #15

---
+++ funcopts/funcopts_test.go
@@ -0,0 +1,67 @@
+package funcopts_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/wafer-bw/funcopts"
+)
+
+func TestProcess(t *testing.T) {
+	t.Parallel()
+
+	t.Run("process all options", func(t *testing.T) {
+		t.Parallel()
+
+		v := &funcopts.T{}
+		number := 1
+		word := "hello"
+
+		if err := funcopts.Process(v, funcopts.WithNumber(number), funcopts.WithWord(word)); err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+
+		if v.GetNumber() != number {
+			t.Errorf("expected %d, got %d", number, v.GetNumber())
+		}
+		if v.GetWord() != word {
+			t.Errorf("expected %s, got '%s'", word, v.GetWord())
+		}
+	})
+
+	t.Run("no error on nil options", func(t *testing.T) {
+		t.Parallel()
+
+		v := &funcopts.T{}
+		number := 1
+		word := "hello"
+
+		if err := funcopts.Process(v, funcopts.WithNumber(number), nil, funcopts.WithWord(word)); err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+	})
+
+	t.Run("no error on empty options", func(t *testing.T) {
+		t.Parallel()
+
+		v := &funcopts.T{}
+
+		if err := funcopts.Process[funcopts.T, funcopts.Option](v); err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+	})
+
+	t.Run("returns error from option", func(t *testing.T) {
+		t.Parallel()
+
+		expectedErr := errors.New("option error")
+		v := &funcopts.T{}
+		o := func(v *funcopts.T) error { return expectedErr }
+
+		if err := funcopts.Process(v, o); err == nil {
+			t.Error("expected error, got nil")
+		} else if err != expectedErr {
+			t.Errorf("expected %s, got %s", expectedErr, err)
+		}
+	})
+}
---
+++ funcopts/go.mod
@@ -0,0 +1,3 @@
+module github.com/wafer-bw/funcopts
+
+go 1.24.2
